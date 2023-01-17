43178705ea6bbb32ed009a14b82fdeba602f13ce
Kvazar7 <Kometa737@gmail.com>
2023-01-17 03:57:44 +0200

    HW-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const objectToSave = {
+    email: "",
+    message: "",
+};
+
+
+savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+emailInput.value = JSON.parse(savedMessage).email || "";
+messageInput.value = JSON.parse(savedMessage).message || "";
+
+// form.addEventListener("input", throttle(addLocalStorageData, 500));
+form.addEventListener("input", addLocalStorageData);
+function addLocalStorageData(e) {
+    const {
+        elements: { email, message }
+    } = e.currentTarget;
+    const savedData = {
+        email: email.value,
+        message: message.value
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(savedData));
+};
+
+form.addEventListener("submit", submitForm);
+function submitForm(e) {
+    e.preventDefault();
+    const {
+    elements: { email, message }
+    } = e.currentTarget;
+    console.log(`email: ${email.value}, message: ${message.value}`);
+    e.currentTarget.reset(),
+    localStorage.clear();
+};
+
+
 
